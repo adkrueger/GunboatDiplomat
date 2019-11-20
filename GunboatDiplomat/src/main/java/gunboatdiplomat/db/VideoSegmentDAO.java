fa77e35893843deece8d42d350d4c69e78b84db9
Ashwin Pai <apai2@wpi.edu>
2019-11-20 18:42:57 -0500

    with getAllVidSeg()

--- GunboatDiplomat/src/main/java/gunboatdiplomat/db/VideoSegmentDAO.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/db/VideoSegmentDAO.java
@@ -1,7 +1,8 @@
 package gunboatdiplomat.db;
 
 import java.sql.*;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import gunboatdiplomat.model.VidSeg;
 
@@ -18,7 +19,7 @@ public class VideoSegmentDAO {
 			connection = null;
 			System.out.println("Connection has failed!");
 		}
-	}// end constructor
+	}
 
 
 	public VidSeg getVidSeg(String id) throws Exception {
@@ -49,19 +50,13 @@ public class VideoSegmentDAO {
 		
 		try {
 			
-			PreparedStatement ps = connection.prepareStatement("INSERT INTO VideoSegment (video_id,character_speaking,quote,season,episode,is_local,is_marked) values(?,?,?,?,?,?,?)");
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO VideoSegment (video_id,character_speaking,quote,season,episode,is_local,is_marked) VALUES(?,?,?,?,?,?,?)");
 			ps.setString(1, vs.id);
 			ps.setString(2, vs.character);
 			ps.setString(3, vs.quote);
 			ps.setInt(4, vs.seasonNum);
 			ps.setInt(5, vs.episodeNum);
-			
-			// Changing a boolean to an int for DB. 
-			if(vs.isLocal == true) {ps.setInt(6, 1);}
-			else {ps.setInt(6, 0);}
-			if(vs.isMarked == true) {ps.setInt(7, 1);}
-			else {ps.setInt(7, 0);}
-			
+			ps.setBoolean(6, vs.isLocal);
 			ps.setBoolean(7, vs.isMarked);
 			ps.execute();
 			return true;
@@ -73,6 +68,30 @@ public class VideoSegmentDAO {
 		
 	}
 
+	public List<VidSeg> getAllVidSegs() throws Exception {
+		
+		List<VidSeg> allVidSegs = new ArrayList<>();
+		
+		try {
+			Statement statement = connection.createStatement();
+			String query = "SELECT * FROM VideoSegment";
+			ResultSet rs = statement.executeQuery(query);
+			
+			while(rs.next()) {
+				VidSeg c = generateVidSeg(rs);
+				allVidSegs.add(c);
+			}
+			rs.close();
+			statement.close();
+			
+			return allVidSegs;
+		}
+		catch(Exception e) {
+			throw new Exception("Failed to get video segments: " + e.getMessage());
+		}
+		
+	}
+	
 	private VidSeg generateVidSeg(ResultSet rs) throws Exception {
 		
 		String id = rs.getString("video_id");
@@ -86,4 +105,4 @@ public class VideoSegmentDAO {
 		return new VidSeg(id, character, quote, season, episode, isLocal, isMarked);
 	}
 
-}//end class VideoSegmentDAO
+}
