a827118a9ea83958dcb45607dbf273d239b7d4b9
adkrueger <adkrueger@wpi.edu>
2019-11-20 12:27:31 -0500

    created addVidSeg

--- GunboatDiplomat/src/main/java/gunboatdiplomat/db/VideoSegmentDAO.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/db/VideoSegmentDAO.java
@@ -2,14 +2,16 @@ package gunboatdiplomat.db;
 
 import java.sql.*;
 
+import gunboatdiplomat.model.VidSeg;
+
 public class VideoSegmentDAO {
+	
 	java.sql.Connection connection;
 
 	public VideoSegmentDAO() {
 		try {
 			connection = DatabaseUtil.connect();
 			System.out.println("Connection has passed!");
-
 		}
 		catch (Exception e) {
 			connection = null;
@@ -33,13 +35,36 @@ public class VideoSegmentDAO {
 
 			return true;
 		}
-
 		catch(Exception e) {
 			System.out.print("string not found!");
 		}
 
 		return false;
 
+	}
+	
+	public boolean addVidSeg(VidSeg vs) throws Exception {
+		
+		try {
+			
+			PreparedStatement ps = connection.prepareStatement("SELECT * FROM VideoSegment where video_id = ?;");
+
+			ps = connection.prepareStatement("INSERT INTO VideoSegment (video_id, character_speaking, quote, season, episode, is_local, is_marked) values(?,?,?,?,?,?,?)");
+			ps.setString(1, vs.id);
+			ps.setString(2, vs.character);
+			ps.setString(3, vs.quote);
+			ps.setInt(4, vs.seasonNum);
+			ps.setInt(5, vs.episodeNum);
+			ps.setBoolean(6, vs.isLocal);
+			ps.setBoolean(7, vs.isMarked);
+			ps.execute();
+			return true;
+			
+		}
+		catch(Exception e) {
+			throw new Exception("Failed to insert video segment: " + e.getMessage());
+		}
+		
 	}
 
 
