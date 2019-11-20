8375c1e7b22b75066fe98a92408853e0ead12746
Ashwin Pai <apai2@wpi.edu>
2019-11-20 16:06:22 -0500

    First test case done babyyyyyyyy!

--- GunboatDiplomat/src/test/java/DatabaseTest/databaseTesting.java
+++ GunboatDiplomat/src/test/java/DatabaseTest/databaseTesting.java
@@ -5,16 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import gunboatdiplomat.db.VideoSegmentDAO;
+import gunboatdiplomat.model.VidSeg;
 
 public class databaseTesting {
 
 	@Test
-	public void testFindVideoSeg() {
-		//int s = 4;
+	public void testFindVideoSeg() throws Exception{
+		VideoSegmentDAO vidsegDAO = new VideoSegmentDAO();
+
+		VidSeg vs1 = new VidSeg("5ecb7cb5-115a-4114-9865-bec03f4b2f5a", "Leonard McCoy", "Death by natural causes.", 2, 12, true, false);
+		VidSeg vs2;
 		
-		VideoSegmentDAO vsg = new VideoSegmentDAO();
-		
-		//vsg.getVideoSegment(s);
+		vs2 = vidsegDAO.getVidSeg("5ecb7cb5-115a-4114-9865-bec03f4b2f5a");
+
+		assertTrue(vs1.equals(vs2));
+
 	}
 
 }
