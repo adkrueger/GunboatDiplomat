49acbf57d8600d37ca0904667e5ad0810244dcb8
adkrueger <adkrueger@wpi.edu>
2019-11-20 17:05:57 -0500

    attempted to work through uploadvidseg, need to figure out how ogg works

--- GunboatDiplomat/src/main/java/gunboatdiplomat/UploadVidSegHandler.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/UploadVidSegHandler.java
@@ -12,8 +12,6 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 
-import edu.wpi.cs.heineman.calculator.http.CreateConstantRequest;
-import edu.wpi.cs.heineman.calculator.http.CreateConstantResponse;
 import gunboatdiplomat.db.VideoSegmentDAO;
 import gunboatdiplomat.http.UploadVidSegRequest;
 import gunboatdiplomat.http.UploadVidSegResponse;
@@ -24,13 +22,13 @@ public class UploadVidSegHandler implements RequestHandler<UploadVidSegRequest, 
 	private AmazonS3 s3 = null;
 
 	LambdaLogger logger;
-	
+
 	// uploading the video segment info to RDS (essentially creating a new video segment)
 	boolean uploadVidSegToRDS(String id, String character, String quote, int seasonNum, int episodeNum, boolean isLocal, boolean isMarked) throws Exception {
-		
+
 		if(logger != null) { logger.log("in uploadVidSegToRDS"); }
 		VideoSegmentDAO dao = new VideoSegmentDAO();
-		
+
 		VidSeg exist = dao.getVidSeg(id);
 		VidSeg vidSeg = new VidSeg(id, character, quote, seasonNum, episodeNum, isLocal, isMarked);
 		if(exist == null) {
@@ -39,60 +37,60 @@ public class UploadVidSegHandler implements RequestHandler<UploadVidSegRequest, 
 		else {
 			return false;
 		}
-		
+
 	}
-	
+
 	boolean uploadVidSegToS3(String id, byte[] contents) throws Exception {
-		
+
 		if(logger != null) { logger.log("in uploadVidSegToS3"); }
-		
+
 		if(s3 == null) {
 			logger.log("attach to S3 request");
 			s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
 			logger.log("attach to s3 successful");
 		}
-		
+
 		ByteArrayInputStream bais = new ByteArrayInputStream(contents);
 		ObjectMetadata omd = new ObjectMetadata();
 		omd.setContentLength(contents.length);
-		
+
 		s3.putObject(new PutObjectRequest("gd3733", "videoSegments/" + id, bais, omd));
-		
+
 		return true;
-		
+
 	}
-	
+
 	@Override 
 	public UploadVidSegResponse handleRequest(UploadVidSegRequest req, Context context)  {
-		
+
 		logger = context.getLogger();
 		logger.log(req.toString());
 
 		UploadVidSegResponse response;
-		
+/*
 		try {
-			byte[] encoded = java.util.Base64.getDecoder().decode(req.base64EncodedValue);
-			if (req.system) {
-				if (uploadVidSegToS3(req.name, encoded)) {
-					response = new UploadVidSegResponse(req.name);
-				} else {
-					response = new UploadVidSegResponse(req.name, 422);
-				}
+			byte[] encodedSeason = java.util.Base64.getDecoder().decode(req.base64EncodedSeason);
+			byte[] encodedEpisode = java.util.Base64.getDecoder().decode(req.base64EncodedEpisode);
+			if (uploadVidSegToS3(req.id, encodedSeason)) {
+				response = new UploadVidSegResponse(req.id);
 			} else {
-				String contents = new String(encoded);
-				double value = Double.valueOf(contents);
-				
-				if (uploadVidSegToRDS(req.name, value)) {
-					response = new UploadVidSegResponse(req.name);
-				} else {
-					response = new UploadVidSegResponse(req.name, 422);
-				}
+				response = new UploadVidSegResponse(req.id, 422);
+			}
+			String contents = new String(encoded);
+			double value = Double.valueOf(contents);
+
+			if (uploadVidSegToRDS(req.id, req.character_speaking, req.quote, Integer.valueOf(req.base64EncodedSeason), Integer.valueOf(req.base64EncodedEpisode), req.isLocal, req.isMarked)) {
+				response = new UploadVidSegResponse(req.id);
+			} else {
+				response = new UploadVidSegResponse(req.id, 422);
 			}
 		} catch (Exception e) {
-			response = new UploadVidSegResponse("Unable to create constant: " + req.name + "(" + e.getMessage() + ")", 400);
+			response = new UploadVidSegResponse("Unable to create constant: " + req.id + "(" + e.getMessage() + ")", 400);
 		}
-
-		return response;
+*/
+		//TODO: aaron fix this
+		return null;	//TODO: Fix this, used to avoid errors
+		//TODO: note to self: fix
 	}
 
 }
