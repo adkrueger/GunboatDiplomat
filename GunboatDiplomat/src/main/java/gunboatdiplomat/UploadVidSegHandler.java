960c12242fcba454fab668e6a149cbcddcf4fc47
adkrueger <adkrueger@wpi.edu>
2019-11-20 12:57:52 -0500

    updated upload handler to take in proper fields

--- GunboatDiplomat/src/main/java/gunboatdiplomat/UploadVidSegHandler.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/UploadVidSegHandler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.s3.AmazonS3;
 
 import gunboatdiplomat.db.VideoSegmentDAO;
+import gunboatdiplomat.model.VidSeg;
 
 public class UploadVidSegHandler {
 
@@ -12,12 +13,12 @@ public class UploadVidSegHandler {
 	LambdaLogger logger;
 	
 	// uploading the video segment (essentially creating a new video segment(
-/*	boolean uploadVidSeg(String id) {
+	boolean uploadVidSeg(String id, String character, String quote, int seasonNum, int episodeNum, boolean isLocal, boolean isMarked) throws Exception {
 		if(logger != null) { logger.log("in uploadVidSeg"); }
 		VideoSegmentDAO dao = new VideoSegmentDAO();
 		
 		VidSeg exist = dao.getVidSeg(id);
-		VidSeg vidSeg = new VidSeg(id);
+		VidSeg vidSeg = new VidSeg(id, character, quote, seasonNum, episodeNum, isLocal, isMarked);
 		if(exist == null) {
 			return dao.addVidSeg(vidSeg);
 		}
@@ -25,5 +26,5 @@ public class UploadVidSegHandler {
 			return false;
 		}
 	}
-*/
+
 }
