3ad09ba0a246ab4929654f313205cd36472eb326
adkrueger <adkrueger@wpi.edu>
2019-11-20 11:35:39 -0500

    added playlist fields and constructor

--- GunboatDiplomat/src/main/java/gunboatdiplomat/model/Playlist.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/model/Playlist.java
@@ -1,5 +1,20 @@
 package gunboatdiplomat.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
 
+	public final String name;
+	public List<VidSeg> vidSegs = new ArrayList<>();
+	
+	public Playlist(String name) {
+		this.name = name;
+	}
+	
+/*	public boolean appendVidSeg(VidSeg vs) {
+		vidSegs.add(vs);
+		return true;
+	}
+*/	
 }
