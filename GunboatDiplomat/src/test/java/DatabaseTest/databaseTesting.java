5b4084c3c0db1be4375b997d0708f28956ac1dc0
Ashwin Pai <apai2@wpi.edu>
2019-11-20 10:35:03 -0500

    This is the first attempt at the DB yet.

---
+++ GunboatDiplomat/src/test/java/DatabaseTest/databaseTesting.java
@@ -0,0 +1,20 @@
+package DatabaseTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import gunboatdiplomat.db.VideoSegmentDAO;
+
+public class databaseTesting {
+
+	@Test
+	public void testFindVideoSeg() {
+		//int s = 4;
+		
+		VideoSegmentDAO vsg = new VideoSegmentDAO();
+		
+		//vsg.getVideoSegment(s);
+	}
+
+}
