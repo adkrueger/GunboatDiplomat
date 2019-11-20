3c8c61e7aedfb8cf0c3305b80e5677c0caee50f5
Ashwin Pai <apai2@wpi.edu>
2019-11-20 16:08:18 -0500

    whoops

--- GunboatDiplomat/src/test/java/DatabaseTest/databaseTesting.java
+++ GunboatDiplomat/src/test/java/DatabaseTest/databaseTesting.java
@@ -8,10 +8,22 @@ import gunboatdiplomat.db.VideoSegmentDAO;
 import gunboatdiplomat.model.VidSeg;
 
 public class databaseTesting {
-
+	
+	
+	// Creating VidSegDAO Object
+	public VideoSegmentDAO vidsegDAO = new VideoSegmentDAO();
+	
+	
+	/**
+	 * testFindVideoSeg
+	 *
+	 * This method tests to see if you can access a video with video_id.
+	 * It runs the getVidSeg(String s) and returns the video obj. with that ID. 
+	 * @throws Exception
+	 * 		In case of failing to connect to DB. 
+	 */
 	@Test
 	public void testFindVideoSeg() throws Exception{
-		VideoSegmentDAO vidsegDAO = new VideoSegmentDAO();
 
 		VidSeg vs1 = new VidSeg("5ecb7cb5-115a-4114-9865-bec03f4b2f5a", "Leonard McCoy", "Death by natural causes.", 2, 12, true, false);
 		VidSeg vs2;
