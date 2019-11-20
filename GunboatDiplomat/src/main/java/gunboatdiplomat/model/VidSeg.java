1a81f6fb0ea7f2667900fa4fd33ca5631f6bb0ed
Shannon Truong <Shannon Truong@DESKTOP-NK7OMQK>
2019-11-20 17:44:18 -0500

    Edit of equals method in video segment model class
    Create the ListVidSegHandler

--- GunboatDiplomat/src/main/java/gunboatdiplomat/model/VidSeg.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/model/VidSeg.java
@@ -22,13 +22,14 @@ public class VidSeg {
 		
 	}
 	
-	public boolean equals(VidSeg vs) {
-		return     this.id == id 
-				&& this.character == character 
-				&& this.quote == quote 
-				&& this.seasonNum == seasonNum 
-				&& this.isLocal == isLocal 
-				&& this.isMarked == isMarked;				
+	@Override
+	public boolean equals(Object o) {
+		return     ((VidSeg)o).id == id 
+				&& ((VidSeg)o).character == character 
+				&& ((VidSeg)o).quote == quote 
+				&& ((VidSeg)o).seasonNum == seasonNum 
+				&& ((VidSeg)o).isLocal == isLocal 
+				&& ((VidSeg)o).isMarked == isMarked;				
 	}
 	
 }
