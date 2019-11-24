1d0292396edfdfc142da44a6d4f2b7e50edf17ba
Ashwin Pai <apai2@wpi.edu>
2019-11-24 13:16:12 -0500

    deletePlaylist

--- GunboatDiplomat/src/main/java/gunboatdiplomat/db/PlaylistDAO.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/db/PlaylistDAO.java
@@ -21,7 +21,7 @@ public class PlaylistDAO {
 		}
 	}
 
-	 
+
 	/**This method uses a playlistname and returns all the VidSeg that are a part of that playlist. 
 	 * 
 	 * @param playlistName
@@ -35,42 +35,45 @@ public class PlaylistDAO {
 		PreparedStatement ps = conn.prepareStatement("SELECT * FROM VideoSegment v JOIN Playlist p WHERE p.video_id = v.video_id AND p.playlist_title = ?;");
 		ps.setString(1, playlistName);
 		ResultSet rs_playlist = ps.executeQuery(); // return all the video_id that are in that playlist. 
-		
-		
+
+
 
 		while(rs_playlist.next()) {
+
 			VidSeg vs = generateVidSeg(rs_playlist);
 			ls.add(vs);
 		}
 		ps.close();
-		
+
 		return ls;
 
 
 	}
-	
-	public boolean deletePlaylis(String playlistName) throws Exception{
-		
+
+	public boolean deletePlaylist(String playlistName) throws Exception{
+
 		//Deleting all songs associated with playlist_title <playlistName>
 		PreparedStatement ps = conn.prepareStatement("DELETE FROM Playlist WHERE playlist_title = ?;");
 		ps.setString(1, playlistName);
 		ResultSet rs = ps.executeQuery();
-		
+
 		ps.close();
-		
+
 		//Checking to see if the playlist still exists. 
 		PreparedStatement ps1 = conn.prepareStatement("SELECT * FROM Playlist WHERE playlist_title = ?;");
 		ps1.setString(1, playlistName);
 		ResultSet rs2 = ps1.executeQuery();
-		
+
 		while(rs2.next()) {
 			return false;
 		}
 		ps1.close();
 		return true;
-		
+
 	}
-	
+
+
+
 	/** This method takes in a ResultSet (aka a row from the DB)
 	 *  and generates a VidSeg obj. from the VideoSegment table.  
 	 * 
@@ -81,7 +84,7 @@ public class PlaylistDAO {
 
 	private VidSeg generateVidSeg(ResultSet rs) throws Exception {
 
-		
+
 		String id = rs.getString(1);
 		String character = rs.getString("character_speaking");
 		System.out.println(id);
@@ -90,7 +93,7 @@ public class PlaylistDAO {
 		int episode = rs.getInt("episode");
 		int isLocal = rs.getInt("is_local");
 		int isMarked = rs.getInt("is_marked");
-		
+
 		System.out.println(id + "has been pulled.");
 
 
