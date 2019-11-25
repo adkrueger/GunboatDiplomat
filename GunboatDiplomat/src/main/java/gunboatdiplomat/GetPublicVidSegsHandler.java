0d60f53e2a4c7746a189016ad719eeec86638b94
Shannon Truong <Shannon Truong@DESKTOP-NK7OMQK>
2019-11-25 18:16:50 -0500

    created the GetVidSegPlaylistReponse and request classes

--- GunboatDiplomat/src/main/java/gunboatdiplomat/GetPublicVidSegsHandler.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/GetPublicVidSegsHandler.java
@@ -1,5 +1,10 @@
 package gunboatdiplomat;
 
-public class GetPublicVidSegsHandler {
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
 
+public class GetPublicVidSegsHandler {
+	private AmazonS3 s3 = null;
+	public LambdaLogger logger;
 }
