510dd4e7e5de24a338bfc194677467079afd7757
Shannon Truong <Shannon Truong@DESKTOP-NK7OMQK>
2019-11-26 00:10:23 -0500

    worked on request and response of get vidseg in playlist

--- GunboatDiplomat/src/main/java/gunboatdiplomat/http/GetVidSegInPlaylistRequest.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/http/GetVidSegInPlaylistRequest.java
@@ -1,5 +1,26 @@
 package gunboatdiplomat.http;
 
 public class GetVidSegInPlaylistRequest {
-
+	public String id;
+	
+	public GetVidSegInPlaylistRequest(String id) {
+		this.id = id;
+	}
+	
+	public GetVidSegInPlaylistRequest() {
+		
+	}
+	
+	public void setID(String id) {
+		this.id = id;
+	}
+	
+	public String getID() {
+		return id;
+	}
+	
+	public String toString() {
+		return "Get(" + id + ")";
+	}
+	
 }
