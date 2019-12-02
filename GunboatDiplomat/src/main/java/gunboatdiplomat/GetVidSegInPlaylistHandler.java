4c0f0c48b49d8e3bc47d9d28b5ab7b1db595403c
adkrueger <adkrueger@wpi.edu>
2019-12-01 23:39:08 -0500

    partially implemented handlers and uploaded to AWS Lambda
    created response https class for getPublicVidSegs

--- GunboatDiplomat/src/main/java/gunboatdiplomat/GetVidSegInPlaylistHandler.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/GetVidSegInPlaylistHandler.java
@@ -1,5 +1,32 @@
 package gunboatdiplomat;
 
-public class GetVidSegInPlaylistHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
 
+import gunboatdiplomat.http.GetVidSegInPlaylistRequest;
+import gunboatdiplomat.http.GetVidSegInPlaylistResponse;
+
+public class GetVidSegInPlaylistHandler implements RequestHandler<GetVidSegInPlaylistRequest, GetVidSegInPlaylistResponse> {
+
+	private AmazonS3 s3 = null;
+	
+	LambdaLogger logger;
+	
+	public GetVidSegInPlaylistResponse handleRequest(GetVidSegInPlaylistRequest request, Context context) {
+		
+		logger = context.getLogger();
+		logger.log(request.toString());
+		
+		GetVidSegInPlaylistResponse response;
+		
+		
+		response = null;		// TODO: Implement and delete this line!!! (just here to satisfy AWS req's)
+		
+		
+		return response;
+		
+	}
+	
 }
