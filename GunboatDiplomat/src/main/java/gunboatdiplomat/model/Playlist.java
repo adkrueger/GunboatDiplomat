efccf09245f719fd4ef574ce8f65206be2fb79d7
Shannon Truong <Shannon Truong@DESKTOP-NK7OMQK>
2019-12-06 13:13:11 -0500

    created test for playlist model class

--- GunboatDiplomat/src/main/java/gunboatdiplomat/model/Playlist.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/model/Playlist.java
@@ -6,31 +6,41 @@ import java.util.List;
 public class Playlist {
 
 	public final String name;
-	public List<VidSeg> contents = new ArrayList<>();
+	public List<VidSeg> contents;
 
 	public Playlist(String name) {
 		this.name = name;
+		this.contents = new ArrayList<>();
 	}
 
-
+	public boolean addVideoSegment(VidSeg vs) {
+		boolean ans = false;
+		contents.add(vs);
+		ans = true; 
+		return ans;
+	}
+	
 	@Override 
 	public boolean equals(Object o) {
 		boolean eq = true;
 		if (((Playlist)o).name.equals(name)) {
 
-			for(int i = 0; i < contents.size(); i++) {
-				if (((Playlist)o).contents.get(i).equals(contents.get(i))) {
-					eq = true;
-				}
-				else {
-					return false;
+			if (((Playlist)o).contents.size() == contents.size()) {
+
+				for(int i = 0; i < contents.size(); i++) {
+					if (((Playlist)o).contents.get(i).equals(contents.get(i))) {
+						eq = true;
+					}
+					else {
+						return false;
+					}
 				}
 			}
 		}
 		else {
 			return false;
 		}
-		
+
 		return eq;
 	}
 }
