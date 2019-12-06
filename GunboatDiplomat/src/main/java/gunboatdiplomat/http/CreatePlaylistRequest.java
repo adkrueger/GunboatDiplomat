856ee26310f99a493024d10cd96ba3aab8b90cd1
Nicole Conill <nconill@wpi.edu>
2019-12-06 12:04:51 -0500

    Create Playlist stuff

--- GunboatDiplomat/src/main/java/gunboatdiplomat/http/CreatePlaylistRequest.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/http/CreatePlaylistRequest.java
@@ -1,5 +1,31 @@
 package gunboatdiplomat.http;
 
 public class CreatePlaylistRequest {
+	
+	public String name;
+	public boolean system;
+	
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	
+	public boolean getSystem() { return system; }
+	public void setSystem(boolean system) { this.system = system; }
+	
+	public CreatePlaylistRequest() {
+		
+	}
+	
+	public CreatePlaylistRequest(String n) {
+		this.name = n;
+	}
+	
+	public CreatePlaylistRequest(String n, boolean system) {
+		this.name = n;
+		this.system = system;
+	}
+	
+	public String toString() {
+		return "CreatePlaylist(" + name + ")";
+	}
 
 }
--- GunboatDiplomat/src/main/java/gunboatdiplomat/http/CreatePlaylistResponse.java
+++ GunboatDiplomat/src/main/java/gunboatdiplomat/http/CreatePlaylistResponse.java
@@ -1,5 +1,31 @@
 package gunboatdiplomat.http;
 
 public class CreatePlaylistResponse {
+	
+	public final String response;
+	public final int httpCode;
+	public final String error;
+	
+	public CreatePlaylistResponse (String s, int code) {
+		this.response = s;
+		this.httpCode = code;
+		this.error = "";
+	}
+	
+	public CreatePlaylistResponse (String s, int code, String error) {
+		this.response = s;
+		this.httpCode = code;
+		this.error = error;
+	}
+	
+	public String toString() {
+		
+		if (httpCode/100 == 2) {
+			return "DeleteResponde(" + response + ")";
+		} else {
+			return "ErrorResult(httpCode = " + httpCode + ", error = " + error + ")";
+		}
+		
+	}
 
 }
