b66707d18aedca3432b96c9978c12d9cdbcdbd06
Shannon Truong <Shannon Truong@DESKTOP-NK7OMQK>
2019-12-10 17:58:34 -0500

    Finished testing ListPlaylistHandlerTest

--- GunboatDiplomat/src/test/java/gunboatdiplomat/HandlerTest/ListPlaylistHandlerTest.java
+++ GunboatDiplomat/src/test/java/gunboatdiplomat/HandlerTest/ListPlaylistHandlerTest.java
@@ -1,5 +1,47 @@
 package gunboatdiplomat.HandlerTest;
 
-public class ListPlaylistHandlerTest {
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import gunboatdiplomat.LambdaTest;
+import gunboatdiplomat.ListPlaylistsHandler;
+import gunboatdiplomat.db.PlaylistDAO;
+import gunboatdiplomat.http.ListPlaylistsResponse;
+import gunboatdiplomat.model.VidSeg;
+
+public class ListPlaylistHandlerTest extends LambdaTest {
+
+	PlaylistDAO dao = new PlaylistDAO();
+
+	@Test
+	public void testListPlaylistHandler() throws Exception {
+
+		dao.createPlaylist("Almost...");
+		dao.createPlaylist("Done...");
+		dao.createPlaylist("With...");
+		dao.createPlaylist("Project!");
+
+		ListPlaylistsHandler handler = new ListPlaylistsHandler();
+		ListPlaylistsResponse resp = handler.handleRequest(null, createContext("list playlist"));
+
+		Assert.assertEquals(200, resp.statusCode);
+
+		HashMap<String, List<VidSeg>> plist = resp.playlists;
+
+		Assert.assertTrue(plist.containsKey("Almost..."));
+		Assert.assertTrue(plist.containsKey("Done..."));
+		Assert.assertTrue(plist.containsKey("With..."));
+		Assert.assertTrue(plist.containsKey("Project!"));
+
+		dao.deletePlaylist("Almost...");
+		dao.deletePlaylist("Done...");
+		dao.deletePlaylist("With...");
+		dao.deletePlaylist("Project!");
+
+	}
 
 }
+
