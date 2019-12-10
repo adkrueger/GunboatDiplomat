79f9901e1abc03228731a7e0eceaa4632fca0ba8
adkrueger <adkrueger@wpi.edu>
2019-12-10 16:30:54 -0500

    search vid segs operational in webapp

--- GunboatDiplomat/js/searchVidSegs.js
+++ GunboatDiplomat/js/searchVidSegs.js
@@ -1,27 +1,72 @@
-function processSearchResponse(response, remoteResponse) {
+function processSearchResponse(response, remoteResponse, characterKeyword, quoteKeyword) {
 
 	console.log(response);
 	console.log(remoteResponse);
 
-	let remoteVidSegs = [];
-	
+	let searchResults = document.getElementById("searchResults");
+
+	let output = "<h3>Search Results</h3><ul class=\"itemList\">";
+
 	let remoteJS = JSON.parse(remoteResponse);
 	for(let m = 0; m < remoteJS.remotes.length; m++) {
 		let url = remoteJS.remotes[m].url;
 		let qIndex = url.indexOf("?apikey=");
 		let search_remote_url = url.substring(0, qIndex);
 		let apikey = url.substring(qIndex+8);
-		
+
+		let remoteVidSegs = [];
+
 		let remoteXHR = new XMLHttpRequest();
 		remoteXHR.open("GET", search_remote_url, true);
 		remoteXHR.setRequestHeader("x-api-key", apikey);
 		remoteXHR.send();
-		
+
 		remoteXHR.onloadend = function() {
 			if(remoteXHR.readyState === XMLHttpRequest.DONE) {
 				if(remoteXHR.status === 200) {
 					console.log("Pulled Remote XHR: " + remoteXHR.responseText);
-					//TODO: need to go through each of these and add them to a list of pulled segments
+					let pulledJS = JSON.parse(remoteXHR.responseText);
+					console.log(pulledJS);
+					for(let n = 0; n < pulledJS.segments.length; n++) {
+						let pulledRemotes = pulledJS.segments;
+						console.log(pulledRemotes[n]);
+						remoteVidSegs.push(pulledRemotes[n]);
+					}
+
+					if(remoteVidSegs.length == 0) {
+						ouput = output + "<p>No remote results found.</p>";
+
+						searchResults.innerHTML = output;
+					}
+					else {
+						for(let x = 0; x < remoteVidSegs.length; x++) {
+							let pulledURL = remoteVidSegs[x]["url"];
+							let pulledCharacter = remoteVidSegs[x]["character"];
+							let pulledText = remoteVidSegs[x]["text"];
+
+							if(characterKeyword !== "" && quoteKeyword !== "") {
+								if(pulledCharacter.toLowerCase().indexOf(characterKeyword.toLowerCase()) !== -1 && pulledText.toLowerCase().indexOf(quoteKeyword.toLowerCase()) !== -1) {
+									output = output + "<li><b>" + pulledURL + "</b><div class=\"vertSpace\"></div>"
+									+ "<span>" + pulledCharacter + ": \"" + pulledText + "\"</span></li><br/>";
+								}
+							}
+							else if(characterKeyword !== "") {
+								if(pulledCharacter.toLowerCase().indexOf(characterKeyword.toLowerCase()) !== -1) {
+									output = output + "<li><b>" + pulledURL + "</b><div class=\"vertSpace\"></div>"
+									+ "<span>" + pulledCharacter + ": \"" + pulledText + "\"</span></li><br/>";
+								}
+							}
+							else {
+								if(pulledText.toLowerCase().indexOf(quoteKeyword.toLowerCase()) !== -1) {
+									output = output + "<li><b>" + pulledURL + "</b><div class=\"vertSpace\"></div>"
+									+ "<span>" + pulledCharacter + ": \"" + pulledText + "\"</span></li><br/>";
+								}
+							}
+
+							searchResults.innerHTML = output;
+						}
+					}
+
 				}
 				else {
 					console.log("remote actual: " + remoteXHR.responseText);
@@ -35,14 +80,11 @@ function processSearchResponse(response, remoteResponse) {
 			}
 		};
 	}
-	
-	let js = JSON.parse(response);
-	let searchResults = document.getElementById("searchResults");
 
-	let output = "<h3>Search Results</h3><ul class=\"itemList\">";
+	let js = JSON.parse(response);
 
 	if(js.vidSegs.length === 0) {
-		output = output + "<p>No results found.</p>"
+		output = output + "<p>No local results found.</p>";
 	}
 	else {
 		for(let i = 0; i < js.vidSegs.length; i++) {
@@ -62,14 +104,11 @@ function processSearchResponse(response, remoteResponse) {
 				output = output + "<li><b>" + id + "</b><div class=\"vertSpace\"></div>"
 				+ "<span>" + charSpeaking + ": \"" + quote + "\"</span></li><br/>";
 			}
-			if(i === js.vidSegs.length-1) {
-				output = output + "</ul>";
-			}
-
 		}
 	}
 	// vidSegs is in response https class of search
 
+	output = output + "</ul>";
 	searchResults.innerHTML = output;
 
 }
@@ -107,7 +146,7 @@ function handleSearchClick() {
 				if(xhr.status === 200 && remoteXHR.status === 200) {
 					console.log("XHR: " + xhr.responseText);
 					console.log("Remote XHR: " + remoteXHR.responseText);
-					processSearchResponse(xhr.responseText, remoteXHR.responseText);
+					processSearchResponse(xhr.responseText, remoteXHR.responseText, form.character.value, form.quote.value);
 				}
 				else if(xhr.status !== 200) {
 					console.log("actual: " + xhr.responseText);
