7b43ca2177265b0185a2ceb929c0cc18f85dea9f
adkrueger <adkrueger@wpi.edu>
2019-12-10 16:51:30 -0500

    added comments and fixed output addition in search

--- GunboatDiplomat/js/searchVidSegs.js
+++ GunboatDiplomat/js/searchVidSegs.js
@@ -8,7 +8,7 @@ function processSearchResponse(response, remoteResponse, characterKeyword, quote
 	let output = "<h3>Search Results</h3><ul class=\"itemList\">";
 
 	let remoteJS = JSON.parse(remoteResponse);
-	for(let m = 0; m < remoteJS.remotes.length; m++) {
+	for(let m = 0; m < remoteJS.remotes.length; m++) {		// go through each remote site
 		let url = remoteJS.remotes[m].url;
 		let qIndex = url.indexOf("?apikey=");
 		let search_remote_url = url.substring(0, qIndex);
@@ -17,7 +17,7 @@ function processSearchResponse(response, remoteResponse, characterKeyword, quote
 		let remoteVidSegs = [];
 
 		let remoteXHR = new XMLHttpRequest();
-		remoteXHR.open("GET", search_remote_url, true);
+		remoteXHR.open("GET", search_remote_url, true);		// get the publicly available segments from the remote
 		remoteXHR.setRequestHeader("x-api-key", apikey);
 		remoteXHR.send();
 
@@ -27,37 +27,36 @@ function processSearchResponse(response, remoteResponse, characterKeyword, quote
 					console.log("Pulled Remote XHR: " + remoteXHR.responseText);
 					let pulledJS = JSON.parse(remoteXHR.responseText);
 					console.log(pulledJS);
-					for(let n = 0; n < pulledJS.segments.length; n++) {
+					for(let n = 0; n < pulledJS.segments.length; n++) {		// go through each segment and add it to our list
 						let pulledRemotes = pulledJS.segments;
-						console.log(pulledRemotes[n]);
 						remoteVidSegs.push(pulledRemotes[n]);
 					}
 
-					if(remoteVidSegs.length == 0) {
+					if(remoteVidSegs.length === 0) {		// usually if there are no remotes registered
 						ouput = output + "<p>No remote results found.</p>";
 
 						searchResults.innerHTML = output;
 					}
 					else {
-						for(let x = 0; x < remoteVidSegs.length; x++) {
+						for(let x = 0; x < remoteVidSegs.length; x++) {		// go through each returned remote
 							let pulledURL = remoteVidSegs[x]["url"];
 							let pulledCharacter = remoteVidSegs[x]["character"];
 							let pulledText = remoteVidSegs[x]["text"];
 
-							if(characterKeyword !== "" && quoteKeyword !== "") {
-								if(pulledCharacter.toLowerCase().indexOf(characterKeyword.toLowerCase()) !== -1 && pulledText.toLowerCase().indexOf(quoteKeyword.toLowerCase()) !== -1) {
+							if(characterKeyword !== "" && quoteKeyword !== "") {		// searching through both character and quote
+								if(pulledCharacter.toLowerCase().indexOf(characterKeyword.toLowerCase()) !== -1 && pulledText.toLowerCase().indexOf(quoteKeyword.toLowerCase()) !== -1) {		// check if our keyword is in the string
 									output = output + "<li><b>" + pulledURL + "</b><div class=\"vertSpace\"></div>"
 									+ "<span>" + pulledCharacter + ": \"" + pulledText + "\"</span></li><br/>";
 								}
 							}
-							else if(characterKeyword !== "") {
-								if(pulledCharacter.toLowerCase().indexOf(characterKeyword.toLowerCase()) !== -1) {
+							else if(characterKeyword !== "") {		// searching through character, not quote
+								if(pulledCharacter.toLowerCase().indexOf(characterKeyword.toLowerCase()) !== -1) {		// check if our keyword is in the string
 									output = output + "<li><b>" + pulledURL + "</b><div class=\"vertSpace\"></div>"
 									+ "<span>" + pulledCharacter + ": \"" + pulledText + "\"</span></li><br/>";
 								}
 							}
-							else {
-								if(pulledText.toLowerCase().indexOf(quoteKeyword.toLowerCase()) !== -1) {
+							else if(quoteKeyword !== "") {		// searching through quote, not character (need if statement here because otherwise it'll add everything by default)
+								if(pulledText.toLowerCase().indexOf(quoteKeyword.toLowerCase()) !== -1) {		// check if our keyword is in the string
 									output = output + "<li><b>" + pulledURL + "</b><div class=\"vertSpace\"></div>"
 									+ "<span>" + pulledCharacter + ": \"" + pulledText + "\"</span></li><br/>";
 								}
@@ -84,7 +83,7 @@ function processSearchResponse(response, remoteResponse, characterKeyword, quote
 	let js = JSON.parse(response);
 
 	if(js.vidSegs.length === 0) {
-		output = output + "<p>No local results found.</p>";
+		output = output + "<p>No results found.</p>";
 	}
 	else {
 		for(let i = 0; i < js.vidSegs.length; i++) {
