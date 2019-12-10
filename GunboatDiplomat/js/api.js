8f6b3a9356442764fb38c5a0050d373177b4f0b4
adkrueger <adkrueger@wpi.edu>
2019-12-09 22:04:16 -0500

    got remote segments url working

--- GunboatDiplomat/js/api.js
+++ GunboatDiplomat/js/api.js
@@ -1,5 +1,5 @@
 // api gateway url
-let api_url = "https://quf3be50n8.execute-api.us-east-2.amazonaws.com/charlie/";
+let api_url = "https://quf3be50n8.execute-api.us-east-2.amazonaws.com/echo/";
 
 let appendVidSeg_url = api_url + "appendVidSeg";			// POST
 let createPlaylist_url = api_url + "createPlaylist";		// POST
@@ -16,3 +16,9 @@ let unregisterRemote_url = api_url + "unregisterRemote";	// POST
 let uploadVidSeg_url = api_url + "uploadVidSeg";			// POST
 
 let vs_url = "https://gd3733.s3.us-east-2.amazonaws.com/videoSegments/";
+
+let remote_url =  "https://5uoy8u1hhh.execute-api.us-east-2.amazonaws.com/RemoteSite/";
+	
+let search_remote_url = remote_url + "publicSegments";
+let apikey = "GgpE67ljkia9Pma3PlgJo8MKOJnxobVZ9mCCLxtq";
+let url_with_key = "https://5uoy8u1hhh.execute-api.us-east-2.amazonaws.com/RemoteSite/publicSegments?apikey=GgpE67ljkia9Pma3PlgJo8MKOJnxobVZ9mCCLxtq";
