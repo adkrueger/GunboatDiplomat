dcfbf0c8c620fba71622862fdc0df236f9d1f946
adkrueger <adkrueger@wpi.edu>
2019-12-10 17:37:27 -0500

    fixed a couple of syntax errors

--- GunboatDiplomat/js/getAllVidSegsAdmin.js
+++ GunboatDiplomat/js/getAllVidSegsAdmin.js
@@ -38,17 +38,11 @@ function processVidSegListResponseAdmin(response) {
 		"\"</span><br/><video width=\"350\" height=\"260\" controls><source src=\"" + url + "\" type=\"video/ogg\"></video>" +
 		"<br/><input class=\"button\" type=\"button\" value=\"Delete\" onclick=\"requestDelete(\'" + id + "\', \'" + charSpeaking + "\', \'" + quote + "\', 1)\"/><div class=\"divider\"></div>" +
 		"<input class=\"button\" type=\"button\" value=\"Mark\" onclick=\"requestMark(\'" + id + "\', 1)\"/><div class=\"divider\"></div>" +
-		"<input class=\"button\" type=\"button\" value=\"Unmark\" onclick=\"requestUnmark(\'" + id + "\', 1)\"/><div class=\"divider\"></div>";
+		"<input class=\"button\" type=\"button\" value=\"Unmark\" onclick=\"requestUnmark(\'" + id + "\', 1)\"/><div class=\"divider\"></div></li><br/><br/>";
 
 		let localString = isLocal ? "True" : "False";
 		let markedString = isMarked ? "True" : "False";
-/*
-		infoOutput = infoOutput + "<li><div id=\"vidSeg-" + id + "-info\"><b>" + id + "</b><br/><span>" + charSpeaking + ": \"" + quote + 
-		"\"</span><br/><span>Season Number: " + seasonNum + "<br/>Episode Number: " + episodeNum + "</span>" +
-		"<br/><span>Local: " + localString + "</span>" +
-		"<br/><span>Marked: " + markedString + "</span>" +
-		"<br/></div></li><br/>";
-*/
+
 		if(isLocal) {	// add the video segment to the list of local segments if necessary
 			localOutput = localOutput + "<li><div id=\"vidSeg-" + id + "-local\"><b>" + id + "</b><br/><span>" + charSpeaking + ": \"" + quote + 
 			"\"</span><br/><span>Marked status: " + markedString + "</span></div></li><br/>";
--- GunboatDiplomat/js/unmark.js
+++ GunboatDiplomat/js/unmark.js
@@ -1,6 +1,6 @@
 function processUnmarkResponse(result, isAdmin){
 	console.log("mark result is: " + result);
-	
+
 	if(isAdmin){
 		refreshVidSegListAdmin();
 	}
@@ -9,42 +9,38 @@ function processUnmarkResponse(result, isAdmin){
 	}
 }
 
-function requestUnmark(vs, isAdmin){ //isAdmin is boolean, and result a string/JSON. 
-	console.log("Unmark result is" + result);
+function requestUnmark(vs, isAdmin){ 		//isAdmin is boolean, and result a string/JSON. 
 
-		let data = {};
-		data["videoID"] = vs
-		
-		let js = JSON.stringify(data);
-		console.log("Mark VidSeg JSL: " + js);
-		
-		let xhr = new XMLHttpRequest();
-		xhr.open("POST", unmarkVidSeg_url, true);
-		xhr.send(js);
-		
-		xhr.onloadend = function() {
-			
-			console.log(xhr);
-			
-			if(xhr.readyState === XMLHttpRequest.DONE){
-				if(xhr.status === 200){
-					console.log("XHR: " + xhr.responseText);
-					processUnmarkResponse(xhr.responseText, isAdmin);
-				}
-				
-				else{
-					console.log("actual: " + xhr.responseText);
-					let newJS = JSON.parse(xhr.responseText);
-					let err = newJS["message"];
-					alert(err)
-				}
+	let data = {};
+	data["videoID"] = vs
+
+	let js = JSON.stringify(data);
+	console.log("Mark VidSeg JSL: " + js);
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", unmarkVidSeg_url, true);
+	xhr.send(js);
+
+	xhr.onloadend = function() {
+
+		console.log(xhr);
+
+		if(xhr.readyState === XMLHttpRequest.DONE){
+			if(xhr.status === 200){
+				console.log("XHR: " + xhr.responseText);
+				processUnmarkResponse(xhr.responseText, isAdmin);
 			}
+
 			else{
-				processUnmarkResponse("N/A", isAdmin);
+				console.log("actual: " + xhr.responseText);
+				let newJS = JSON.parse(xhr.responseText);
+				let err = newJS["message"];
+				alert(err)
 			}
-			
 		}
+		else{
+			processUnmarkResponse("N/A", isAdmin);
+		}
+
 	}
-	
-	
-} 
+}
